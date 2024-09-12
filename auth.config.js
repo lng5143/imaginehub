0e5c9a40ac37c1ae24f83f7af4d71b406cb22c9f
lamnguyen <nguyenphuonglam96@gmail.com>
2024-09-12 11:13:26 +0700

    google provider + allowDangerousEmailAccountLinking

--- auth.config.js
+++ auth.config.js
@@ -3,8 +3,9 @@ import Google from "next-auth/providers/google";
 const authConfig = {
     providers: [
         Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+            allowDangerousEmailAccountLinking: true,
         })
     ]
 }
