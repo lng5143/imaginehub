832df088ca73bdf5b8d994adc87a8a0517e1b635
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-01 23:52:42 +0700

    refactor provider + model

--- components/create/create-inputs.js
+++ components/create/create-inputs.js
@@ -2,16 +2,19 @@ import ModelSelector from "./model-selector";
 import { useCurrentModel } from "@/store/use-current-model";
 import DallEForm from "./dall-e-form";
 import StableDiffusionForm from "./stable-diffusion-form";
+import { getProvider } from "@/lib/utils";
 
 export default function CreateInputs() {
   const [currentModel] = useCurrentModel();
+
+  const provider = getProvider(currentModel);
 
   return (
     <div className="flex flex-col w-[360px] p-5 bg-gray-100 gap-10 h-auto">
         <ModelSelector />
         <div className="mt-auto">
-            {currentModel === "dall-e-3" || currentModel === "dall-e-2" ? <DallEForm /> : null}
-            {currentModel === "stable-diffusion" ? <StableDiffusionForm /> : null}
+            {provider === "openai" ? <DallEForm /> : null}
+            {provider === "stability" ? <StableDiffusionForm /> : null}
         </div>
     </div>
   );
--- components/create/model-selector.js
+++ components/create/model-selector.js
@@ -4,15 +4,21 @@ import { useCurrentModel } from "@/store/use-current-model";
 export default function ModelSelector() {
     const [currentModel, setCurrentModel] = useCurrentModel();
 
+    const handleSelectModel = (model) => {
+        
+    }
+
     return (
         <Select value={currentModel} onValueChange={setCurrentModel}>
             <SelectTrigger>
                 <SelectValue placeholder="Select a model" />
             </SelectTrigger>
             <SelectContent>
-                <SelectItem value="dall-e-3">DALL-E 3</SelectItem>
-                <SelectItem value="dall-e-2">DALL-E 2</SelectItem>
-                <SelectItem value="stable-diffusion">Stable Diffusion</SelectItem>
+                <SelectItem value="de-3">DALL-E 3</SelectItem>
+                <SelectItem value="de-2">DALL-E 2</SelectItem>
+                <SelectItem value="sd-3">Stable Diffusion 3</SelectItem>
+                <SelectItem value="si-ultra">Stable Image Ultra</SelectItem>
+                <SelectItem value="si-core">Stable Image Core</SelectItem>
             </SelectContent>
         </Select>
     );
