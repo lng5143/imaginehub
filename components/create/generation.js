cafab65763ba5d0fb5a5a6f30c25d3ff984c2018
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-03 16:01:16 +0700

    edit generation component

--- components/create/generation.js
+++ components/create/generation.js
@@ -23,68 +23,16 @@ export default function Generation({ data }) {
         )
     }
 
-    if (data.samples === 1) {
-        return (
-            <div className="hover:cursor-pointer">
-                <Image
-                    src={thumbnail}
-                    width={272}
-                    height={272}
-                    alt="Top image"
-                    className="relative z-30 rounded-md shadow-lg"
-                />
-            </div>
-        )
-    }
-
-    if (data.samples === 2) {
-        return (
-            <div className="relative hover:cursor-pointer">
-                <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-background z-10">
-                    <Image
-                        src={thumbnail}
-                        width={264}
-                        height={264}
-                        alt="Top image"
-                        className="relative z-30 rounded-md shadow-lg w-[96%] h-[96%]"
-                    />
-                    <Image
-                        src={thumbnail}
-                        width={264}
-                        height={264}
-                        alt="Middle image"
-                        className="absolute top-[4%] left-[4%] z-20 rounded-md shadow-lg opacity-75 w-[96%] h-[96%]"
-                        />
-                </div>
-            </div>
-        )
-    }
-
     return (
         <div className="relative hover:cursor-pointer">
-            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-background z-10">
+            <div className="flex z-40 items-center justify-center absolute top-2 right-2 bg-white text-gray-800 border-2 border-gray-800 p-1 rounded-full w-6 h-6 text-xs font-semibold">{data.samples}</div>
             <Image
                 src={thumbnail}
-                width={256}
-                height={256}
-                alt="Top image"
-                className="relative z-30 rounded-md shadow-lg w-[92%] h-[92%]"
+                width={272}
+                height={272}
+                alt={data?.prompt}
+                className="relative w-full h-full z-30 rounded-md shadow-lg"
             />
-            <Image
-                src={thumbnail}
-                width={256}
-                height={256}
-                alt="Middle image"
-                className="absolute top-[4%] left-[4%] z-20 rounded-md shadow-lg opacity-75 w-[92%] h-[92%]"
-            />
-            <Image
-                src={thumbnail}
-                width={256}
-                height={256}
-                alt="Bottom image"
-                className="absolute top-[8%] left-[8%] z-10 rounded-md shadow-lg opacity-50 w-[92%] h-[92%]"
-            />
-            </div>
         </div>
     )
 }
