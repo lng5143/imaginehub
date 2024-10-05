c08739089c590f85ddb1e0c3178cd12165ff4f93
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-05 18:36:23 +0700

    validateGenerateImage

--- server/actions/users.js
+++ server/actions/users.js
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/server/lib/prisma";
 import { auth } from "@/auth";
+import { UserTier } from "@prisma/client";
 
 export const getCurrentUserInfo = async () => {
     const session = await auth();
@@ -15,4 +16,20 @@ export const getCurrentUserInfo = async () => {
     })
 
     return user;
+}
+
+export const validateGenerateImage = async (userId) => {
+    const user = await prisma.user.findUnique({
+        where: { id: userId }
+    })
+
+    if (!user) {
+        return { error: "User not found" }
+    }
+
+    if (user.trialCredits <= 0 && user.tier === UserTier.FREE) {
+        return { error: "You have run out of trial credits" }
+    }
+
+    return { success: true }
 }
