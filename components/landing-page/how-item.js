a1ade17a2981e15fba825e3d1a152879d4b87221
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-08 18:24:03 +0700

    how section styling

--- components/landing-page/how-item.js
+++ components/landing-page/how-item.js
@@ -1,6 +1,6 @@
 export default function HowItem({ title, description, img }) {
     return (
-        <div className="flex flex-col gap-5 basis-1/3 rounded-lg p-16 items-center">
+        <div className="flex flex-col gap-5 basis-1/3 rounded-lg p-12 items-center">
             <img src={img} alt={title} className="w-10 h-10" />
             <div className="flex flex-col gap-2 text-center">
                 <h3 className="text-lg font-bold">{title}</h3>
--- components/landing-page/step-number.js
+++ components/landing-page/step-number.js
@@ -3,12 +3,12 @@ import { cn } from "@/lib/utils"
 export default function StepNumber({ variant, number }) {
     return (
         <div className="relative w-full">
-            <div className="flex w-full h-2">
-                <div className={cn("basis-1/2", variant === "left" ? "bg-transparent" : "bg-indigo-950")} />
-                <div className={cn("basis-1/2", variant === "right" ? "bg-transparent" : "bg-indigo-950")} />
+            <div className="flex w-full h-1.5">
+                <div className={cn("basis-1/2", variant === "left" ? "bg-transparent" : "bg-indigo-200")} />
+                <div className={cn("basis-1/2", variant === "right" ? "bg-transparent" : "bg-indigo-200")} />
             </div>
             <div 
-                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 shadow-lg bg-amber-500 text-black text-2xl font-bold flex items-center justify-center rounded-full">
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 shadow-lg bg-indigo-950 text-white text-xl font-bold flex items-center justify-center rounded-full">
                 {number}
             </div>
         </div>
