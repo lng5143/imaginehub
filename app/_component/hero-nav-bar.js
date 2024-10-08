13484458ad19263892c234d5c9779aec24f12804
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-08 16:19:55 +0700

    button shadow + navbar scroll effect

--- app/_component/hero-nav-bar.js
+++ app/_component/hero-nav-bar.js
@@ -1,13 +1,34 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function HeroNavBar() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
   return (
-    <div className="w-full px-20 py-3 fixed top-0 left-0 flex items-center bg-transparent z-10 backdrop-blur-sm">
+    <div className={cn("w-full px-20 py-3 fixed top-0 left-0 flex items-center bg-transparent z-10 ", {
+      "border-b border-gray-200 shadow-xl backdrop-blur-md": isScrolled,
+    })}>
       <Link href="/" className="flex items-center flex-grow">
         <img src="/logo/logo-dark-text.png" alt="logo" className="w-40" />
       </Link>
-      <Button className="bg-indigo-950 text-white hover:bg-amber-500 hover:text-slate-950 hover:scale-105 transition-all duration-300">
+      <Button className="shadow-xl bg-indigo-950 text-white hover:bg-amber-500 hover:text-slate-950 hover:scale-105 transition-all duration-300">
           <Link href="/pricing">Pricing</Link>
       </Button>
     </div>
