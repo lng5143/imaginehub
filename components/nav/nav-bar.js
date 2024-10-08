1d00a2568343c657a1295342639f2cee34830b95
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-08 16:01:50 +0700

    create nav bar

--- components/nav/nav-bar.js
+++ components/nav/nav-bar.js
@@ -3,16 +3,22 @@
 import Login from "../auth/login-dialog";
 import useCurrentUser from "@/hooks/use-current-user";
 import UserButton from "./user-button";
+import Link from "next/link";
 
 export default function NavigationBar() {
   const user = useCurrentUser();
 
   return (
-    <div className="flex p-2 gap-10 justify-end bg-indigo-950 text-white">
-      {!user && <Login />}
-      {user && 
-        <UserButton user={user} />
-      }
+    <div className="flex py-2 px-4 items-center bg-indigo-950 text-white">
+      <Link href="/create" className="flex-grow items-center">
+        <img src="/logo/logo-white.png" alt="logo" className="w-10" />
+      </Link>
+      <div className="flex gap-10 items-center ">
+        {!user && <Login />}
+        {user && 
+          <UserButton user={user} />
+        }
+      </div>
     </div>
   );
 }
