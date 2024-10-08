6c06ee65cb7abedc464f22026f1000afc47ccee8
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-08 15:56:24 +0700

    added logo

--- app/_component/hero-nav-bar.js
+++ app/_component/hero-nav-bar.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 export default function HeroNavBar() {
   return (
-    <div className="w-full px-10 py-3 fixed top-0 left-0 flex justify-end bg-transparent z-10 backdrop-blur-sm">
+    <div className="w-full px-20 py-3 fixed top-0 left-0 flex items-center bg-transparent z-10 backdrop-blur-sm">
+      <Link href="/" className="flex items-center flex-grow">
+        <img src="/logo/logo-dark-text.png" alt="logo" className="w-40" />
+      </Link>
       <Button className="bg-indigo-950 text-white hover:bg-amber-500 hover:text-slate-950 hover:scale-105 transition-all duration-300">
           <Link href="/pricing">Pricing</Link>
       </Button>
--- components/landing-page/footer.js
+++ components/landing-page/footer.js
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 export default function FooterSection() {
     return (
-        <div className="flex flex-col px-20 w-full">
-            <div className="flex gap-20 w-full py-10 px-32">
+        <div className="flex flex-col px-32 w-full">
+            <div className="flex gap-20 w-full py-10">
                 <div className="flex-grow">
-                    <Image src="/logo.svg" alt="logo" width={100} height={100} />
+                    <img src="/logo/logo-dark-text.png" alt="logo" className="w-60" />
                     {/* <div className="flex gap-5">
                         <Image src="/instagram.svg" alt="instagram" width={20} height={20} />
                         <Image src="/facebook.svg" alt="facebook" width={20} height={20} />
@@ -28,7 +28,7 @@ export default function FooterSection() {
                 </div>
             </div>
             <hr className="w-full border-slate-300" />
-            <div className="flex gap-5 py-5 px-20 text-sm text-gray-800">
+            <div className="flex gap-5 py-5 text-sm text-gray-800">
                 <Link href="/privacy">Privacy Policy</Link>
                 <Link href="/terms">Terms & Conditions</Link>
                 <Link href="/refund">Refund Policy</Link>
