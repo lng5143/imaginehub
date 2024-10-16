cb2c9a8bc0797dd5514fc04747b01fa1cf217f75
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-16 22:53:43 +0700

    update package + redis URL

--- server/lib/redis.js
+++ server/lib/redis.js
@@ -1,8 +1,16 @@
 import { Redis } from 'ioredis';
 
-export const connection = new Redis({
+let connection;
+
+if (process.env.NODE_ENV === 'development') {
+    connection = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD || '',
     maxRetriesPerRequest: null
-});
+    });
+} else {
+    connection = new Redis(process.env.REDIS_URL);
+}
+
+export { connection };
