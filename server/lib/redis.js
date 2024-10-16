33a4aff84e33774db53fed65f1639fd6552fad78
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-16 22:57:38 +0700

    redis url

--- server/lib/redis.js
+++ server/lib/redis.js
@@ -2,15 +2,17 @@ import { Redis } from 'ioredis';
 
 let connection;
 
-if (process.env.NODE_ENV === 'development') {
-    connection = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-    password: process.env.REDIS_PASSWORD || '',
-    maxRetriesPerRequest: null
-    });
+if(process.env.REDIS_URL) {
+    connection = new Redis(process.env.REDIS_URL, {maxRetriesPerRequest: null});
 } else {
-    connection = new Redis(process.env.REDIS_URL);
+    connection = new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD || '',
+        maxRetriesPerRequest: null
+    });
 }
+
+console.log(connection);
 
 export { connection };
