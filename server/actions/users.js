f10b88d6930b0fca55643ec771eda2199c26efb6
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-18 23:17:40 +0700

    generate api server validation

--- server/actions/users.js
+++ server/actions/users.js
@@ -3,6 +3,7 @@
 import { prisma } from "@/server/lib/prisma";
 import { auth } from "@/auth";
 import { UserTier } from "@prisma/client";
+import { DE2FormSchema, DE3FormSchema, SD3FormSchema, SICoreFormSchema, SIUltraFormSchema } from "@/schemas";
 
 export const getCurrentUserInfo = async () => {
     const session = await auth();
@@ -18,7 +19,7 @@ export const getCurrentUserInfo = async () => {
     return user;
 }
 
-export const validateGenerateImage = async (userId) => {
+export const validateGenerateImage = async (userId, data) => {
     const user = await prisma.user.findUnique({
         where: { id: userId }
     })
@@ -31,7 +32,34 @@ export const validateGenerateImage = async (userId) => {
         return { success: false, message: "You have run out of trial credits. Please upgrade to continue generating images." }
     }
 
-    return { success: true }
+    let validatedFields;
+    switch (data.model) {
+        case "de-2": 
+            validatedFields = DE2FormSchema.safeParse(data);
+            break;
+        case "de-3":
+            validatedFields = DE3FormSchema.safeParse(data);
+            break;
+        case "sd3-medium":
+        case "sd3-large":
+        case "sd3-large-turbo":
+            validatedFields = SD3FormSchema.safeParse(data);
+            break;
+        case "si-core":
+            validatedFields = SICoreFormSchema.safeParse(data);
+            break;
+        case "si-ultra":
+            validatedFields = SIUltraFormSchema.safeParse(data);
+            break;
+        default:
+            return { success: false, message: "Invalid model" }
+    }
+
+    if (!validatedFields.success) {
+        return { success: false, message: validatedFields.error.errors[0].message }
+    }
+
+    return { success: true, data: validatedFields.data }
 }
 
 export const updateUserCredits = async (userId) => {
