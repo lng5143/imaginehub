e57e909abdac4124c84704d4c0868b1a2d0eb3e8
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-22 14:52:52 +0700

    blog apis

--- server/actions/blogs.js
+++ server/actions/blogs.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { CreatePostSchema } from "@/schemas";
 import { prisma } from "../lib/prisma";
 import { BLOG_PAGE_SIZE } from "@/const/imagine-box-consts";
 
@@ -31,17 +32,60 @@ export async function getBlogPostById(id) {
 }
 
 export async function createOrEditBlogPost(data) {
+    const validateRes = validateBlogPostData(data);
 
+    if (!validateRes.success) {
+        return validateRes;
+    }
+
+    if (!data.id) 
+        return await createBlogPost(data);
+    else 
+        return await editBlogPost(data);
 }
 
 async function createBlogPost(data) {
+    const post = await prisma.blogPost.create({
+        data
+    });
 
+    return { success: true, message: "Post created successfully" };
 }
 
 async function editBlogPost(data) {
+    const post = await prisma.blogPost.update({
+        where: { id: data.id },
+        data
+    });
 
+    return { success: true, message: "Post updated successfully" };
 }
 
-function validateBlogPostData(data) {
+async function validateBlogPostData(data) {
+    const validatedFields = CreatePostSchema.safeParse(data);
 
+    if (!validatedFields.success) {
+        return { success: false, message: validatedFields.error.message };
+    }
+
+    const existingPost = await getBlogPostBySlug(data.slug);
+
+    if (!data.id && existingPost) {
+        return { success: false, message: "Slug already exists" };
+    }
+
+    if (!isValidHTML(data.content)) {
+        return { success: false, message: "Invalid HTML content" };
+    }
+
+    return { success: true };
 }
+
+function isValidHTML(htmlString) {
+    try {
+      const dom = new JSDOM(htmlString);
+      return !!dom.window.document.body; 
+    } catch (e) {
+      return false;
+    }
+  }
