a4c0391d6831d4583eab49840dfbff2dc42c88c6
lamnguyen <nguyenphuonglam96@gmail.com>
2024-10-23 18:59:39 +0700

    how to links

--- components/landing-page/hero.js
+++ components/landing-page/hero.js
@@ -2,6 +2,8 @@
 
 import CTAButton from "./cta-button";
 import Image from "next/image";
+import Link from "next/link";
+
 export default function HeroSection() {
     return (
         <div className="bg-[url('/hero-bg.jpg')] flex flex-col lg:flex-row lg:gap-16 items-center bg-cover bg-center bg-no-repeat w-full">
@@ -10,7 +12,10 @@ export default function HeroSection() {
                     Simple tool to use your favorite image generation model
                 </h1>
                 <p>Enter your API key and start generating images. No subscription required.</p>
-                <CTAButton />
+                <div className="flex flex-col gap-2">
+                    <CTAButton />
+                    <Link href="/how-to" className="text-gray-700 text-sm underline">How to use</Link>
+                </div>
             </div>
             <div className="hidden lg:block pr-20 py-20 lg:py-40 basis-2/3">
                 <Image src="/hero-image.png" alt="hero-image" 
