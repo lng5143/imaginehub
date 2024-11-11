b034e384d0b2092bc435c6f34fdba2bf0902b752
lamnguyen <nguyenphuonglam96@gmail.com>
2024-11-11 11:45:18 +0700

    model selector tsx

--- components/create/model-selector.tsx
+++ components/create/model-selector.tsx
@@ -11,50 +11,19 @@ import {
 } from "@/components/ui/dialog";
 import {VisuallyHidden} from "@radix-ui/react-visually-hidden";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export default function ModelSelector() {
     const [currentModel, setCurrentModel] = useCurrentModel();
-    const [openModelSelector, setOpenModelSelector] = useState(false);
+    const [open, setOpen] = useState(false);
 
+    const handleSelectModel = (model: any) => {
+        setCurrentModel(model);
+        setOpen(false);
+    }   
 
     return (
-        // <Popover open={openModelSelector} onOpenChange={setOpenModelSelector}>
-        //     <PopoverTrigger asChild>
-        //         <Button variant="outline" role="combobox" aria-expanded={openModelSelector} className="w-full justify-between text-sm shadow-lg">
-        //             {currentModel.name ? currentModel.name : "Select a model"}
-        //         </Button>
-        //     </PopoverTrigger>
-        //     <PopoverContent align="start" className="w-80 shadow-lg p-2">
-        //         <Command>
-        //             <CommandInput className="text-sm" placeholder="Search model..." />
-        //             <CommandList>
-        //                 <CommandEmpty><p className="text-sm">No model found.</p></CommandEmpty>
-        //                 <CommandGroup className="">
-        //                     {MODELS.map((model) => (
-        //                         <CommandItem
-        //                             className="flex items-center text-sm"
-        //                             key={model.code}
-        //                             value={model.name}
-        //                             onSelect={() => {
-        //                                 setCurrentModel(model)
-        //                                 setOpenModelSelector(false)
-        //                             }}
-        //                         >
-        //                             <p className="flex-grow">{model.name}</p>
-        //                             <Check
-        //                                 className={cn(
-        //                                 "mr-2 h-4 w-4",
-        //                                 currentModel.code === model.code ? "opacity-100" : "opacity-0"
-        //                                 )}
-        //                             />
-        //                         </CommandItem>
-        //                     ))}
-        //                 </CommandGroup>
-        //             </CommandList>
-        //         </Command>
-        //     </PopoverContent>
-        // </Popover>
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <Button>Select Model</Button>
             </DialogTrigger>
@@ -67,7 +36,11 @@ export default function ModelSelector() {
                 </DialogHeader>
                 <div className="p-2 grid grid-cols-3">
                     {MODELS.map((model) => (
-                        <div key={model.code} className="flex items-center justify-center">
+                        <div 
+                            key={model.code} 
+                            className={cn("flex items-center justify-center", model.code === currentModel.code ? "border border-blue-500" : "")}
+                            onClick={() => handleSelectModel(model)}
+                        >
                             {model.name}
                         </div>
                     ))}
