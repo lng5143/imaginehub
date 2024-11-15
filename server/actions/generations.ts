f1e16ad712fc7c8cbb6f97796914c26a8cc43638
lamnguyen <nguyenphuonglam96@gmail.com>
2024-11-15 11:41:32 +0700

    generation & upload APIs

--- server/actions/generations.ts
+++ server/actions/generations.ts
@@ -1,165 +1,15 @@
 "use server"
 
 import { prisma } from "@/server/lib/prisma";
-import { auth } from "@/auth";
 import { PAGE_SIZE } from "@/const/consts";
 import { updateUserCredits } from "./users";
 import { uploadFileToS3AndGetUrl } from "../lib/aws";
 import { Image, ImageGeneration, ImageGenerationStatus, Provider } from "@prisma/client";
-import { getCurrentUserId, getUserById } from "../lib/user";
+import { getCurrentUserId } from "../lib/user";
 import { ResponseFactory } from "@/types/response";
 import { ApiResponse } from "@/types/response";
 import { PagedData } from "@/types/paged-data";
 import { CreateOrEditImageGenerationDTO } from "@/types/image-generation";
-
-export const createOrEditImageGeneration = async (data: CreateOrEditImageGenerationDTO) => {
-    let existingGen;
-    if (data.id)
-        existingGen = await getImageGenerationById(data.id);
-
-    const validationRes = await validateImageGenerationData(data, existingGen);
-    if (!validationRes.success || !validationRes.data)
-        return validationRes;
-
-    const validatedData = validationRes.data;
-
-    if (validatedData.id)
-        return await editImageGeneration(validatedData!, existingGen);
-
-    return await createImageGeneration(validatedData!);
-}
-
-const validateImageGenerationData = async (data: CreateOrEditImageGenerationDTO, existingGen: ImageGeneration)
-    : Promise<ApiResponse<CreateOrEditImageGenerationDTO>> => {
-    const userId = await getCurrentUserId();
-
-    if (!userId)
-        return ResponseFactory.fail({ message: "Unauthorized" });
-
-    data.userId = userId;
-
-    return ResponseFactory.success({ data: data });
-}
-
-const createImageGeneration = async (data: CreateOrEditImageGenerationDTO): Promise<ApiResponse<ImageGeneration>> => {
-    const { id, openAIGenerationConfigs, stabilityGenerationConfigs, ...genData} = data;
-
-    if (data.provider === Provider.OPENAI) {
-        const generation = await prisma.imageGeneration.create({
-            data: {
-                openAIGenerationConfigs : openAIGenerationConfigs,
-                ...genData
-            }
-        })
-
-        return ResponseFactory.success({ data: generation });
-    }
-
-    if (data.provider === Provider.STABILITY) {
-        const generation = await prisma.imageGeneration.create({
-            data: {
-                stabilityGenerationConfigs : stabilityGenerationConfigs,
-                ...genData
-            }
-        })
-
-        return ResponseFactory.success({ data: generation });
-    }
-
-    return ResponseFactory.fail({ message: "Invalid provider" });
-}
-
-const editImageGeneration = async (data: CreateOrEditImageGenerationDTO, existingGen: ImageGeneration) => {
-    // TODO: Implement
-}
-
-const getImageGenerationById = async (genId: string) => {
-    return await prisma.imageGeneration.findUnique({
-        where: { id: genId }
-    })
-}
-
-const updateImageGenerationStatus = async (genId: string, status: ImageGenerationStatus): Promise<ApiResponse> => {
-    const res = await prisma.imageGeneration.update({
-        where: { id: genId },
-        data: { status }
-    })
-
-    return ResponseFactory.success({ data: res });
-}
-
-export const insertInitialGeneration = async (data: CreateOrEditImageGenerationDTO) => {
-    const response = await prisma.imageGeneration.create({
-        data: {
-            ...data
-        }
-    })
-
-    return { success: true, data: response }
-}
-
-export const uploadImageAndUpdateGeneration = async (genId: string, provider: Provider, data: any) : Promise<ApiResponse> => {
-    const uploadRes = await uploadImages(genId, provider, data);
-    if (!uploadRes.success) {
-        await updateImageGenerationStatus(genId, ImageGenerationStatus.FAILED);
-        return uploadRes;
-    }
-
-    await updateImageGenerationStatus(genId, ImageGenerationStatus.COMPLETED);
-    await updateUserCredits();
-
-    return ResponseFactory.success({ message: "Image generation completed!" });
-}
-
-const uploadImages = async (genId: string, provider: string, data: any) => {
-    let imageUrls: string[] = [];
-
-    if (provider === "openai") {
-        for (const item of data) {
-            const response = await fetch(item.url);
-            if (!response.ok) 
-                continue;
-
-            const buffer = await response.arrayBuffer();
-
-            const urlRes = await uploadFileToS3AndGetUrl(process.env.AWS_S3_BUCKET, genId, buffer);
-            if (!urlRes.success || !urlRes.data) {
-                console.log("error uploading image", urlRes);
-                continue;
-            }
-
-            imageUrls = [...imageUrls, urlRes.data];
-        }
-    }
-
-    if (provider === "stability") {
-        const imageBlobs = data.values();
-
-        for (const item of imageBlobs) {
-            const buffer = await item.arrayBuffer();
-
-            const urlRes = await uploadFileToS3AndGetUrl(process.env.AWS_S3_BUCKET, genId, buffer);
-            if (!urlRes.success || !urlRes.data) {
-                console.log("error uploading image", urlRes);
-                continue;
-            }
-
-            imageUrls = [...imageUrls, urlRes.data];
-        }
-    }
-
-    if (imageUrls.length === 0)
-        return { success: false, message: "Failed to save images." }
-
-    const res = await prisma.image.createMany({
-        data: imageUrls.map(url => ({
-            url: url,
-            imageGenerationId: genId
-        }))
-    })
-
-    return { success: true, data: res }
-}
 
 export const getGenerations = async (page: number): Promise<ApiResponse<PagedData<ImageGeneration>>> => {
     const userId = await getCurrentUserId();
@@ -197,31 +47,163 @@ export const getGenerations = async (page: number): Promise<ApiResponse<PagedDat
     return ResponseFactory.success({ data: { totalCount: totalCount, data: generations }})
 }
 
-export const getGenerationDetails = async (generationId: string): Promise<ApiResponse<ImageGeneration & { images: Image[] } >> => {
+const getImageGenerationById = async (genId: string, includeImages: boolean = false) : Promise<ApiResponse<ImageGeneration>> => {
     const userId = await getCurrentUserId();
     
     if (!userId)
         return ResponseFactory.fail({ message: "User not found" })
 
     const generation = await prisma.imageGeneration.findUnique({
-        where: { id: generationId },
+        where: { id: genId },
         include: {
-            images: true
+            images: includeImages
         }
     })
 
-    // const imagesRes = await getImages(generationId);
-
     return ResponseFactory.success({ data: generation })
+
 }
 
-// const getImages = async (generationId) => {
-//     const images = await prisma.image.findMany({
-//         where: { imageGenerationId: generationId }
-//     })
+export const createOrEditImageGeneration = async (data: CreateOrEditImageGenerationDTO) => {
+    let existingGen;
+    if (data.id) {
+        const existingGenRes = await getImageGenerationById(data.id);
+        if (!existingGenRes.success || !existingGenRes.data)
+            return existingGenRes;
 
-//     return { success: true, data: images }
-// }
+        existingGen = existingGenRes.data;
+    }
+
+    const validationRes = await validateImageGenerationData(data, existingGen);
+    if (!validationRes.success || !validationRes.data)
+        return validationRes;
+
+    const validatedData = validationRes.data;
+
+    if (validatedData.id)
+        return await editImageGeneration(validatedData!, existingGen!);
+
+    return await createImageGeneration(validatedData!);
+}
+
+const validateImageGenerationData = async (data: CreateOrEditImageGenerationDTO, existingGen: ImageGeneration | undefined)
+    : Promise<ApiResponse<CreateOrEditImageGenerationDTO>> => {
+    const userId = await getCurrentUserId();
+
+    if (!userId)
+        return ResponseFactory.fail({ message: "Unauthorized" });
+
+    if (data.id && !existingGen)
+        return ResponseFactory.fail({ message: "Old image generation not found" });
+
+    data.userId = userId;
+
+    return ResponseFactory.success({ data: data });
+}
+
+const createImageGeneration = async (data: CreateOrEditImageGenerationDTO): Promise<ApiResponse<ImageGeneration>> => {
+    const { id, openAIGenerationConfigs, stabilityGenerationConfigs, ...genData} = data;
+
+    if (data.provider === Provider.OPENAI) {
+        const generation = await prisma.imageGeneration.create({
+            data: {
+                openAIGenerationConfigs : { create: openAIGenerationConfigs },
+                ...genData
+            }
+        })
+
+        return ResponseFactory.success({ data: generation });
+    }
+
+    if (data.provider === Provider.STABILITY) {
+        const generation = await prisma.imageGeneration.create({
+            data: {
+                stabilityGenerationConfigs : { create: stabilityGenerationConfigs },
+                ...genData
+            }
+        })
+
+        return ResponseFactory.success({ data: generation });
+    }
+
+    return ResponseFactory.fail({ message: "Invalid provider" });
+}
+
+const editImageGeneration = async (data: CreateOrEditImageGenerationDTO, existingGen: ImageGeneration) => {
+    // TODO: Implement when needed
+}
+
+const updateImageGenerationStatus = async (genId: string, status: ImageGenerationStatus): Promise<ApiResponse> => {
+    const res = await prisma.imageGeneration.update({
+        where: { id: genId },
+        data: { status }
+    })
+
+    return ResponseFactory.success({ data: res });
+}
+
+export const uploadImageAndUpdateGeneration = async (genId: string, provider: Provider, data: any) : Promise<ApiResponse> => {
+    const uploadRes = await uploadImages(genId, provider, data);
+    if (!uploadRes.success) {
+        await updateImageGenerationStatus(genId, ImageGenerationStatus.FAILED);
+        return uploadRes;
+    }
+
+    await updateImageGenerationStatus(genId, ImageGenerationStatus.COMPLETED);
+    await updateUserCredits();
+
+    return ResponseFactory.success({ message: "Image generation completed!" });
+}
+
+const uploadImages = async (genId: string, provider: string, data: any) : Promise<ApiResponse> => {
+    let imageUrls: string[] = [];
+
+    if (provider === "openai") {
+        for (const item of data) {
+            const response = await fetch(item.url);
+            if (!response.ok) 
+                continue;
+
+            const buffer = await response.arrayBuffer();
+
+            const urlRes = await uploadFileToS3AndGetUrl(process.env.AWS_S3_BUCKET, genId, buffer);
+            if (!urlRes.success || !urlRes.data) {
+                console.log("error uploading image", urlRes);
+                continue;
+            }
+
+            imageUrls = [...imageUrls, urlRes.data];
+        }
+    }
+
+    if (provider === "stability") {
+        const imageBlobs = data.values();
+
+        for (const item of imageBlobs) {
+            const buffer = await item.arrayBuffer();
+
+            const urlRes = await uploadFileToS3AndGetUrl(process.env.AWS_S3_BUCKET, genId, buffer);
+            if (!urlRes.success || !urlRes.data) {
+                console.log("error uploading image", urlRes);
+                continue;
+            }
+
+            imageUrls = [...imageUrls, urlRes.data];
+        }
+    }
+
+    if (imageUrls.length === 0)
+        return ResponseFactory.fail({ message: "Failed to upload images." });
+
+    const res = await prisma.image.createMany({
+        data: imageUrls.map(url => ({
+            url: url,
+            imageGenerationId: genId
+        }))
+    })
+
+    return ResponseFactory.success({ });
+}
 
 export const deleteGeneration = async (generationId: string): Promise<ApiResponse> => {
     const userId = await getCurrentUserId();
--- server/lib/aws.ts
+++ server/lib/aws.ts
@@ -1,4 +1,4 @@
-import { ApiResponse } from "@/types/response";
+import { ApiResponse, ResponseFactory } from "@/types/response";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import { fromEnv } from '@aws-sdk/credential-provider-env'
@@ -8,7 +8,7 @@ const s3Client = new S3Client({
     credentials: fromEnv()
 });
 
-export const uploadFileToS3AndGetUrl = async (bucketName: string | undefined, genId: string, file: ArrayBuffer) : Promise<ApiResponse> => {
+export const uploadFileToS3AndGetUrl = async (bucketName: string | undefined, genId: string, file: ArrayBuffer) : Promise<ApiResponse<string>> => {
     
     try {
         const fileName = `${genId}/${uuidv4()}`;
@@ -22,12 +22,11 @@ export const uploadFileToS3AndGetUrl = async (bucketName: string | undefined, ge
         const res = await s3Client.send(command);
 
         if (res.$metadata.httpStatusCode !== 200) {
-            return { success: false, message: "Failed to upload file" };
+            return ResponseFactory.fail({ message: "Failed to upload image" });
         }
 
-        return { success: true, data: `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}` };
-
+        return ResponseFactory.success({ data:`https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`});
     } catch (error) {
-        return { success: false, message: "Failed to upload image" };
+        return ResponseFactory.fail({ message: "Failed to upload image" });
     }
 }
