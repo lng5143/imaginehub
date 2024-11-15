d0b8ec0ecfb780f56c9b59c2fe0d1e9475253ea0
lamnguyen <nguyenphuonglam96@gmail.com>
2024-11-15 10:32:54 +0700

    model libs + consts delete

--- const/consts.js
+++ const/consts.js
@@ -1,59 +1,11 @@
 export const SD_PRESETS = ["3d-model", "analog-film", "anime", "cinematic", "comic-book", "digital-art", "enhance", "fantasy-art", "isometric", "line-art", "low-poly", "modeling-compound", "neon-punk", "origami", "photographic", "pixel-art", "tile-texture"]
 export const SD_RATIOS = ["1:1", "2:3", "3:2", "4:5", "5:4", "9:16", "9:21", "16:9", "21:9"]
-export const SD3_MODELS = ["sd3-large", "sd3-turbo", "sd3-medium"]
 export const DE2_SIZES = ["256x256", "512x512", "1024x1024"];
 export const DE3_SIZES = ["1024x1024", "1024x1792", "1792x1024"];
 export const DE3_QUALITIES = ["standard", "hd"];
 export const DE2_QUALITIES = ["standard"];
-export const MODELS = [
-    {
-        code: "de-2",
-        provider: "openai",
-        name: "DALL-E 2",
-    },
-    {
-        code: "de-3",
-        provider: "openai",
-        name: "DALL-E 3",
-    },
-    {
-        code: "sd3-medium",
-        provider: "stability",
-        name: "Stable Diffusion 3 Medium",
-    },
-    {
-        code: "sd3-large",
-        provider: "stability",
-        name: "Stable Diffusion 3 Large",
-    },
-    {
-        code: "sd3-large-turbo",
-        provider: "stability",
-        name: "Stable Diffusion 3 Turbo",
-    },
-    {
-        code: "si-core",
-        provider: "stability",
-        name: "Stable Image Core",
-    },
-    {
-        code: "si-ultra",
-        provider: "stability",
-        name: "Stable Image Ultra",
-    }
-]
 
-export const PROVIDERS = {
-    openai: {
-        code: "openai",
-        name: "OpenAI",
-    },
-    stability: {
-        code: "stability",
-        name: "Stability AI",
-    }
-}
-
+// TODO: determine if IMAGE_BUCKET_NAME, IMAGINEBOX_OTP_PRICE_ID should be env var
 export const IMAGE_BUCKET_NAME = "generated-images";
 
 export const PAGE_SIZE = 24;
---
+++ lib/models.ts
@@ -0,0 +1,49 @@
+import { Model, Provider } from "@prisma/client"
+
+export const getProviderFromModel = (model: Model) : Provider | undefined => {
+    switch (model) {
+        case Model.DALL_E_2:
+        case Model.DALL_E_3:
+            return Provider.OPENAI
+        case Model.STABLE_DIFFUSION_3_MEDIUM:
+        case Model.STABLE_DIFFUSION_3_LARGE:
+        case Model.STABLE_DIFFUSION_3_LARGE_TURBO:
+        case Model.STABLE_IMAGE_CORE:
+        case Model.STABLE_IMAGE_ULTRA:
+            return Provider.STABILITY
+        default:
+    }       return undefined;
+}
+
+export const getProviderName = (provider: Provider) : string | undefined => {
+    switch (provider) {
+        case Provider.OPENAI:
+            return "OpenAI"
+        case Provider.STABILITY: 
+            return "Stability AI"
+        default:
+            return undefined;
+
+    }
+}
+
+export const getModelName = (model: Model) : string | undefined => {
+    switch (model) {
+        case Model.DALL_E_2:
+            return "DALL-E 2";
+        case Model.DALL_E_3:
+            return "DALL-E 3";
+        case Model.STABLE_DIFFUSION_3_LARGE:
+            return "Stable Diffusion 3 Large";
+        case Model.STABLE_DIFFUSION_3_MEDIUM:
+            return "Stable Diffusion 3 Medium";
+        case Model.STABLE_DIFFUSION_3_LARGE_TURBO:
+            return "Stable Diffusion 3 Large Turbo";
+        case Model.STABLE_IMAGE_CORE:
+            return "Stable Image Core";
+        case Model.STABLE_IMAGE_ULTRA:
+            return "Stable Image Ultra";
+        default:
+            return undefined;
+    }
+}
