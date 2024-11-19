a78363963260310ec470df06ce91e35afc6ab3cc
lamnguyen <nguyenphuonglam96@gmail.com>
2024-11-19 10:57:55 +0700

    useGenerationState indexed DB

--- hooks/use-generation-state.ts
+++ hooks/use-generation-state.ts
@@ -1,28 +1,38 @@
 import { LSConsts } from "@/const/consts";
-import { Provider } from "@prisma/client";
+import { GenerationState } from "@/types/generation-state";
+import { openDB } from 'idb';
 
-interface GenerationState {
-    id: string;
-    status: 'PROCESSING' | 'UPLOADING' | 'COMPLETED' | 'FAILED';
-    provider: Provider;
-    prompt: string;
-    startedAt: number;
-}
+export const useGenerationState = async () => {
 
-export const useGenerationState = () => {
+    const db = await openDB('ib-pending-generations', 1, {
+        upgrade(db) {
+            db.createObjectStore('images');
+        }
+    })
 
-    const savePendingGenerations = ( state: GenerationState, images: Blob[] ) => {
+    const savePendingGenerations = async ( state: GenerationState, images: Blob[] ) => {
         const pending = JSON.parse(localStorage.getItem(LSConsts.PENDING_GENERATIONS) || '[]');
         localStorage.setItem(LSConsts.PENDING_GENERATIONS, JSON.stringify([...pending, state ]));
 
         // TODO: save images to indexDB 
+        if (images) {
+            await Promise.all(
+                images.map(async (image, index) => {
+                    await db.put('images', image, state.id + '-' + index);
+                })
+            )
+        }
     }
 
-    const removePendingGeneration = ( id: string ) => {
+    const removePendingGeneration = async ( id: string ) => {
         const pending = JSON.parse(localStorage.getItem(LSConsts.PENDING_GENERATIONS) || '[]');
         localStorage.setItem(LSConsts.PENDING_GENERATIONS, JSON.stringify(pending.filter((gen: GenerationState) => gen.id !== id)));
 
         // TODO: remove images from indexDB
+        const keys = await db.getAllKeys('images');
+        await Promise.all(
+            keys.filter(key => key.toString().startsWith(id)).map(key => db.delete('images', key))
+        )
     }
 
     return { savePendingGenerations, removePendingGeneration };
