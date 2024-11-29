29c2dd7440d630f1708cce087c72e6777517d599
lamnguyen <nguyenphuonglam96@gmail.com>
2024-11-29 15:21:55 +0700

    docker compose deployment

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM node:18-alpine AS builder
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npx prisma generate
+
+RUN npm run build
+
+FROM node:18-alpine AS runner
+
+WORKDIR /app
+
+ENV NODE_ENV production
+# ENV PORT 3000
+
+RUN npm install -g pm2
+
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/pm2.config.cjs ./
+COPY --from=builder /app/public ./public
+COPY --from=builder /app/.next ./.next
+COPY --from=builder /app/node_modules ./node_modules
+
+# EXPOSE ${PORT}
+EXPOSE 3000
+
+CMD ["pm2-runtime", "start", "pm2.config.cjs"]
--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
             name: 'nextjs-app',
             script: 'npm',
             args: 'run start',
+            error_file: '/app/logs/app/error.log',
+            out_file: '/app/logs/app/out.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
+            merge_logs: true,
+            max_size: '10M',
+            retain: '30',
             env: {
                 NODE_ENV: 'production'
             }
@@ -12,6 +18,12 @@ module.exports = {
             name: 'payment-worker',
             script: 'node',
             args: 'worker/payment-worker.js',
+            error_file: '/app/logs/worker/error.log',
+            out_file: '/app/logs/worker/out.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
+            merge_logs: true,
+            max_size: '10M',
+            retain: '30',
             env: {
                 NODE_ENV: 'production'
             }
